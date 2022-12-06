75140a71996b2617bf8a4f091899a3c465a24731
Yash86400 <knowledge86400@gmail.com>
2022-12-06 17:35:30 +0530

    Number showing in first input auto focus on first input and when I hit action keys the data shows up on result input, that's great

--- index.js
+++ index.js
@@ -21,3 +21,48 @@ function filterField(e) {
   let badValues = /[^\d\/\+\-\*\.]/gi;
   t.value = t.value.replace(badValues, "")
 }
+
+
+// let btn_element = document.getElementsByClassName("number-keys");
+// btn_element.addEventListener("click", ()=>{
+//   document.getElementsByClassName("handle-data").innerHTML = "Boss has joined the party."
+// })
+
+const allButtons = document.getElementsByClassName("keys")
+
+for (let index = 0; index < allButtons.length; index++) {
+  allButtons[index].addEventListener("click", function (event) {
+    // console.log(event.target.value);
+    nowLetsHandleTheChicken(event.target.value)
+  })
+
+}
+
+let listOfValuesPressed = []
+
+function nowLetsHandleTheChicken(value) {
+  let valueOne = "";
+  let valueTwo = "";
+  // Need 2 functions, first for showing the data on screen and second for back processing of those to show on top box as an answer.
+  // console.log(value);
+  if (value == "-" || value == "+" || value == "=" || value == "x" || value == "/") {
+    // console.log("Jai Shree Ram");
+    if (valueTwo.length == 0) {
+      // console.log("Hi")
+      console.log(listOfValuesPressed);
+      valueTwo = listOfValuesPressed[listOfValuesPressed.length - 1]
+      document.querySelector("input:nth-child(1)").value = valueTwo
+    } else if (valueOne.length > 0) {
+
+    }
+    return;
+  }
+  else {
+    document.querySelector("input:nth-child(2)").value += value
+    // valueOne += document.querySelector("input:nth-child(2)").value
+    // use list and append and then join parseInt
+    listOfValuesPressed.push(document.querySelector("input:nth-child(2)").value)
+  }
+
+  // console.log(valueOne);
+}
